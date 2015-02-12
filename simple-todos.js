f2a813ab241a83faa6cece1eadee116c8575e6fc
Emily Owaki <emily.owaki@gmail.com>
2015-02-12 13:56:06 -0600

    adding css styling, changing tasks to stuff to understand what's going on

--- simple-todos.js
+++ simple-todos.js
@@ -3,9 +3,9 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
+      { stuff: "Walk the dog" },
+      { stuff: "Do the laundry" },
+      { stuff: "Eat all the snacks" }
     ]
   });
 }
