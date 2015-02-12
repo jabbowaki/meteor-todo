141437ccc9c8d7adb85b1706f23b794be053bf0f
Emily Owaki <emily.owaki@gmail.com>
2015-02-12 16:09:07 -0600

    hides completed tasks

--- simple-todos.js
+++ simple-todos.js
@@ -4,8 +4,16 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function() {
+      if (Session.get("hideCompleted")){
+        //if hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
     }
   });
   //attaches submit event to template
@@ -24,6 +32,10 @@ if (Meteor.isClient) {
     event.target.text.value = "";
     //Prevent default form submit
     return false;
+  },
+
+  "change .hide-completed input": function(event){
+    Session.set("hideCompleted", event.target.checked);
   }
 });
 
