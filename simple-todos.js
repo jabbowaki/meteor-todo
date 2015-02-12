9e2009276e8ec538fccc5cddbfa429de9f40a0cb
Emily Owaki <emily.owaki@gmail.com>
2015-02-12 15:06:48 -0600

    commented code, also now sorts by most recent

--- simple-todos.js
+++ simple-todos.js
@@ -5,13 +5,17 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+  //attaches submit event to template
   Template.body.events({
+    //listens to the submit event on anything with class new task, passes in event
   "submit .new-task": function(event){
+    // event.target is form element, text is equal to the value of form input
     var text = event.target.text.value;
-
+    // console.log(event)
+    // console.log(text)
     Tasks.insert({
       text: text,
       createdAt: new Date()
