8a78edf010c51d45263c12b1bc51fe74922470f2
Emily Owaki <emily.owaki@gmail.com>
2015-02-12 14:24:09 -0600

    add form to header, updates with new tasks now

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,25 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { stuff: "Walk the dog" },
-      { stuff: "Do the laundry" },
-      { stuff: "Eat all the snacks" }
-    ]
+    tasks: function(){
+      return Tasks.find({});
+    }
   });
+  Template.body.events({
+  "submit .new-task": function(event){
+    var text = event.target.text.value;
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date()
+    });
+    //clears form
+    event.target.text.value = "";
+    //Prevent default form submit
+    return false;
+  }
+});
 }
